b1c8088a060d16aa5603cb583e3ad56c4b4c2ce3
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-25 00:05:49 +0200

    Added JobTypes template webAPI

--- Controllers/JobTypesController.cs
+++ Controllers/JobTypesController.cs
@@ -11,5 +11,35 @@ namespace All4SA.Controllers
         {
             return new ObjectResult("");
         }
+
+        [HttpGet("GetJobTypeByID/{jobTypeID}", Name = "GetJobTypeByID")]
+        public IActionResult GetJobTypeByID(int jobTypeID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddJobType", Name = "AddJobType")]
+        public IActionResult AddJobType(string jobTypeName, int hourlyRate)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateJobType/{jobTypeID}", Name = "UpdateJobType")]
+        public IActionResult UpdateJobType(int jobTypeID, string jobTypeName, int hourlyRate)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteJobType/{jobTypeID}", Name = "SoftDeleteJobType")]
+        public IActionResult SoftDeleteJobType(int jobTypeID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteJobType/{jobTypeID}", Name = "DeleteJobType")]
+        public IActionResult DeleteJobType(int jobTypeID)
+        {
+            return new ObjectResult("");
+        }
     }
 }
