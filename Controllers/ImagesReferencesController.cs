39b679b9ef3608c51bdbd4f2704b3361cdcd4ac6
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-24 23:30:44 +0200

    Added ImagesReferences template webAPI

--- Controllers/ImagesReferencesController.cs
+++ Controllers/ImagesReferencesController.cs
@@ -7,7 +7,37 @@ namespace All4SA.Controllers
     public class ImagesReferencesController : Controller
     {
         [HttpGet(Name = "GetAllImagesReferences")]
-        public IActionResult Index()
+        public IActionResult GetAllImagesReferences()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpGet("GetImageReferenceByID/{imageReferenceID}", Name = "GetImageReferenceByID")]
+        public IActionResult GetImageReferenceByID(int imageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddImageReference", Name = "AddImageReference")]
+        public IActionResult AddImageReference(string ImageReference)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateImageReference/{imageReferenceID}", Name = "UpdateImageReference")]
+        public IActionResult UpdateImageReference(int imageReferenceID, string ImageReference)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteImageReference/{imageReferenceID}", Name = "SoftDeleteImageReference")]
+        public IActionResult SoftDeleteImageReference(int imageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteImageReference/{imageReferenceID}", Name = "DeleteImageReference")]
+        public IActionResult DeleteImageReference(int imageReferenceID)
         {
             return new ObjectResult("");
         }
