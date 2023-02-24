3887dc0b50b722cecaca697039e7acb065db3514
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-25 01:05:18 +0200

    Added AddressesCRUD skeleton class

---
+++ Database/DatabaseActionsBridge.cs
@@ -0,0 +1,18 @@
+﻿using static ProjectManagement.Database.DatabaseActions;
+
+namespace ProjectManagement.Database
+{
+    public abstract class DatabaseActionsBridge : DatabaseActions
+    {
+        protected static readonly string currentDirectory = Directory.GetCurrentDirectory();
+
+        public static object GetByID(int ID) => new();
+        public static object GetAll() => new();
+        public static DatabaseActionsResponses InsertEntry(object newEntry) => DatabaseActionsResponses.Failed;
+
+        public static DatabaseActionsResponses UpdateEntryByID(int ID, object updateEntry) => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses DeleteEntryByID(int ID) => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses DeleteAll() => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses SoftDeleteEntryByID(int ID) => DatabaseActionsResponses.Failed;
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using ProjectManagement.Database;
+using static ProjectManagement.Database.DatabaseActions;
+
+namespace All4SA.Models
+{
+    public class Address : DatabaseActionsBridge
+    {
+        public static Address GetByID(int ID)
+        {
+            return 
+        };
+        public static object GetAll() => new();
+        public static DatabaseActionsResponses InsertEntry(object newEntry) => DatabaseActionsResponses.Failed;
+
+        public static DatabaseActionsResponses UpdateEntryByID(int ID, object updateEntry) => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses DeleteEntryByID(int ID) => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses DeleteAll() => DatabaseActionsResponses.Failed;
+        public static DatabaseActionsResponses SoftDeleteEntryByID(int ID) => DatabaseActionsResponses.Failed;
+    }
+}
