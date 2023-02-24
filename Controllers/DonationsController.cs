67ede4ad8681794702dfa35a58b1fe3763a6fdf8
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-24 23:22:18 +0200

    Added Donations template webAPI

--- Controllers/DonationsController.cs
+++ Controllers/DonationsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
 
 namespace All4SA.Controllers
 {
@@ -8,6 +9,36 @@ namespace All4SA.Controllers
     {
         [HttpGet(Name = "GetAllDonations")]
         public IActionResult GetAllDonations()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpGet("GetDonationByID/{donationID}", Name = "GetDonationByID")]
+        public IActionResult GetDonationByID(int donationID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddDonation", Name = "AddDonation")]
+        public IActionResult AddDonation(int userID, int amount, int jobRequestID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateDonation/{donationID}", Name = "UpdateDonation")]
+        public IActionResult UpdateDonation(int donationID, int userID, int amount, int jobRequestID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteDonation/{donationID}", Name = "SoftDeleteDonation")]
+        public IActionResult SoftDeleteDonation(int donationID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteDonation/{donationID}", Name = "DeleteDonation")]
+        public IActionResult DeleteDonation(int donationID)
         {
             return new ObjectResult("");
         }
