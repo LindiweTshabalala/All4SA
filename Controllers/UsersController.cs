f5f7441ed6109ac69dba980022e277b7b2d544d4
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-25 00:29:54 +0200

    Added Users template webAPI

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,5 +11,35 @@ namespace All4SA.Controllers
         {
             return new ObjectResult("");
         }
+
+        [HttpGet("GetUserByID/{userID}", Name = "GetUserByID")]
+        public IActionResult GetUserByID(int userID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddUser", Name = "AddUser")]
+        public IActionResult AddUser(string firstName, string surname, string identityCode, string loginToken)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateUser/{userID}", Name = "UpdateUser")]
+        public IActionResult UpdateUser(int userID, string firstName, string surname, string identityCode, string loginToken)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteUser/{userID}", Name = "SoftDeleteUser")]
+        public IActionResult SoftDeleteUser(int userID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteUser/{userID}", Name = "DeleteUser")]
+        public IActionResult DeleteUser(int userID)
+        {
+            return new ObjectResult("");
+        }
     }
 }
