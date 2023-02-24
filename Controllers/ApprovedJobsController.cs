12c2bd477ba73445e764842a87d886f631973a5d
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-24 23:03:30 +0200

    Added JobsImagesReferences template webAPI

---
+++ Controllers/ApprovedJobsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace All4SA.Controllers
+{
+    [ApiController]
+    [Route("ApprovedJobs")]
+    public class ApprovedJobsController : Controller
+    {
+        [HttpGet(Name = "GetAllApprovedJobss")]
+        public IActionResult GetAllApprovedJobss()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpGet("GetApprovedJobByID/{jobID}", Name = "GetApprovedJobsByID")]
+        public IActionResult GetApprovedJobsByID(int jobID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddApprovedJob", Name = "AddApprovedJob")]
+        public IActionResult AddApprovedJob(int jobRequestID, int userID, int statusID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateApprovedJob/{jobID}", Name = "UpdateApprovedJob")]
+        public IActionResult UpdateApprovedJob(int jobID, int jobRequestID, int userID, int statusID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteApprovedJob/{jobID}", Name = "SoftDeleteApprovedJob")]
+        public IActionResult SoftDeleteApprovedJob(int jobID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteApprovedJob/{jobID}", Name = "ApprovedJob")]
+        public IActionResult DeleteApprovedJob(int jobID)
+        {
+            return new ObjectResult("");
+        }
+    }
+}
---
+++ Controllers/JobsImagesReferencesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace All4SA.Controllers
+{
+    [ApiController]
+    [Route("JobsImagesReferences")]
+    public class JobsImagesReferencesController : Controller
+    {
+        [HttpGet(Name = "GetAllJobsImagesReferences")]
+        public IActionResult GetAllJobsImagesReferences()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpGet("GetJobsImageReferenceByID/{jobsImageReferenceID}", Name = "JobsImageReferenceByID")]
+        public IActionResult GetJobsImageReferenceReferenceByID(int jobsImageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddJobsImageReference", Name = "AddJobsImageReference")]
+        public IActionResult AddJobsImageReferenceReference(int jobID, int ImageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateJobsImageReference/{jobsImageReferenceID}", Name = "UpdateJobsImageReference")]
+        public IActionResult UpdateJobsImagesReference(int jobsImageReferenceID, int jobID, int ImageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteJobsImageReference/{jobsImageReferenceID}", Name = "SoftDeleteJobsImageReference")]
+        public IActionResult SoftDeleteJobsImageReference(int jobsImageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteJobsImageReference/{jobsImageReferenceID}", Name = "JobsImageReference")]
+        public IActionResult DeleteJobsImageReference(int jobsImageReferenceID)
+        {
+            return new ObjectResult("");
+        }
+    }
+}
