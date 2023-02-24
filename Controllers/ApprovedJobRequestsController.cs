c1c37ea8d8e0738c5d7929f26bb02153da88e74e
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-24 22:04:54 +0200

    Added ApprovedJobRequests template webAPI

--- Controllers/ApprovedJobRequestsController.cs
+++ Controllers/ApprovedJobRequestsController.cs
@@ -11,5 +11,35 @@ namespace All4SA.Controllers
         {
             return new ObjectResult("");
         }
+
+        [HttpGet("GetApprovedJobRequestByID/{jobID}", Name = "GetApprovedJobRequestByID")]
+        public IActionResult GetApprovedJobRequestByID(int jobID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddApprovedJobRequest", Name = "AddApprovedJobRequest")]
+        public IActionResult AddApprovedJobRequest(int jobRequestID, int userID, int statusID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateApprovedJobRequest/{jobID}", Name = "UpdateApprovedJobRequest")]
+        public IActionResult UpdateApprovedJobRequest(int jobID, int jobRequestID, int userID, int statusID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteApprovedJobRequest/{jobID}", Name = "SoftDeleteApprovedJobRequest")]
+        public IActionResult SoftDeleteApprovedJobRequest(int jobID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteApprovedJobRequest/{jobID}", Name = "ApprovedJobRequest")]
+        public IActionResult DeleteApprovedJobRequest(int addressID)
+        {
+            return new ObjectResult("");
+        }
     }
 }
