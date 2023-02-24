5c0fe4d6245ae17327da604a6bc6e34aea5e56db
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-24 23:42:29 +0200

    Added JobRequests template webAPI

--- Controllers/JobRequestsController.cs
+++ Controllers/JobRequestsController.cs
@@ -7,7 +7,37 @@ namespace All4SA.Controllers
     public class JobRequestsController : Controller
     {
         [HttpGet(Name = "GetAllJobRequests")]
-        public IActionResult Index()
+        public IActionResult GetAllJobRequests()
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpGet("GetJobRequestByID/{jobRequestID}", Name = "GetJobRequestByID")]
+        public IActionResult GetJobRequestByID(int jobRequestID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPost("AddJobRequest", Name = "AddJobRequest")]
+        public IActionResult AddJobRequest(string jobRequestDescription, int UserID, int imageReferenceID, string jobTypeID, int estimatedCost)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("UpdateJobRequest/{jobRequestID}", Name = "UpdateJobRequest")]
+        public IActionResult UpdateJobRequest(int jobRequestID, string jobRequestDescription, int UserID, int imageReferenceID, string jobTypeID, int estimatedCost)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpPut("SoftDeleteJobRequest/{jobRequestID}", Name = "SoftDeleteJobRequest")]
+        public IActionResult SoftDeleteJobRequest(int jobRequestID)
+        {
+            return new ObjectResult("");
+        }
+
+        [HttpDelete("DeleteJobRequest/{jobRequestID}", Name = "DeleteJobRequest")]
+        public IActionResult DeleteJobRequest(int jobRequestID)
         {
             return new ObjectResult("");
         }
