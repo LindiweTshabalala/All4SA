4d9703e6776a40fb9da0293371b829a3d703cb99
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-25 22:41:26 +0200

    Made Changes to Database and Models

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,41 +1,8 @@
 ﻿using All4SA.Database;
-using static All4SA.Database.DatabaseActions;
 
 namespace All4SA.Models
 {
     public class Address : DatabaseActionsBridge
     {
-        public static new Address GetByID(int ID)
-        {
-            return new Address();
-        }
-
-        public static new List<Address> GetAll()
-        {
-            return new List<Address>();
-        }
-
-        public static DatabaseActionsResponses InsertEntry(Address newEntry)
-        {
-            return DatabaseActionsResponses.Failed;
-        }
-
-        public static DatabaseActionsResponses UpdateEntryByID(int ID, Address updateEntry)
-        {
-            return DatabaseActionsResponses.Failed;
-        }
-
-        public static new DatabaseActionsResponses DeleteEntryByID(int ID)
-        {
-            return DatabaseActionsResponses.Failed;
-        }
-        public static new DatabaseActionsResponses DeleteAll()
-        {
-            return DatabaseActionsResponses.Failed;
-        }
-        public static new DatabaseActionsResponses SoftDeleteEntryByID(int ID)
-        {
-            return DatabaseActionsResponses.Failed;
-        }
     }
 }
