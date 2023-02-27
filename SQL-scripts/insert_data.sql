241ce86dde4a91eaa493d39fac5681917d1df40d
amaze <amaze.mkhonto2@gmail>
2023-02-27 12:27:18 +0200

    Fixed insert data script

--- SQL-scripts/insert_data.sql
+++ SQL-scripts/insert_data.sql
@@ -1,3 +1,16 @@
+INSERT INTO JobTypes (JobTypeName, HourlyRate)
+VALUES 
+   ('Landscaper', 20.00),
+   ('Construction Worker', 25.00),
+   ('Electrician', 30.00),
+   ('Plumber', 30.00),
+   ('Farmhand', 15.00),
+   ('Community watch', 18.00),
+   ('Waste Management Worker', 20.00),
+   ('Road Maintenance Worker', 22.00);
+   
+   
+
 
 INSERT INTO Users (FirstName, Surname, IdNumber, Token)
 VALUES 
@@ -11,13 +24,9 @@ VALUES
   ('Sarah', 'Taylor', '9208084002081', 'efg234hij567'),
   ('Michael', 'Wilson', '8710093007087', 'klm789nop123'),
   ('Karen', 'Anderson', '8801102003081', 'qrs234tuv567');
-
-INSERT INTO Statuses (StatusName)
-VALUES
-	('Complete'),
-	('In Progress'),
-	('Incomplete');
-	
+  
+  
+  
 INSERT INTO Contacts (Email, CellPhone, UserID)
 VALUES 
   ('john.doe@example.com', '0811234567', 1),
@@ -30,9 +39,9 @@ VALUES
   ('sarah.taylor@example.com', '0888901234', 8),
   ('michael.wilson@example.com', '0899012345', 9),
   ('karen.anderson@example.com', '0800123456', 10);
- 
- 
-INSERT INTO Addresses (Province, City, StreetName, PostalCode, UserID)
+  
+  
+  INSERT INTO Addresses (Province, City, StreetName, PostalCode, UserID)
 VALUES 
   ('Gauteng', 'Johannesburg', 'Main Street', 2001, 1),
   ('Western Cape', 'Cape Town', 'Church Street', 8001, 2),
@@ -46,7 +55,7 @@ VALUES
   ('Gauteng', 'Pretoria', 'Vermeulen Street', 0001, 10);
   
   
-INSERT INTO ImageReferences (ImageReference) 
+  INSERT INTO ImageReferences (ImageReference) 
 VALUES 
 	('https://example.com/images/image1.jpg'),
 	('https://example.com/images/image2.jpg'),
@@ -57,19 +66,10 @@ VALUES
 	('https://example.com/images/image7.jpg'),
 	('https://example.com/images/image8.jpg'),
 	('https://example.com/images/image9.jpg'),
-	('https://example.com/images/image10.jpg'); 
+	('https://example.com/images/image10.jpg');
 	
-INSERT INTO JobTypes (JobTypeName, HourlyRate)
-VALUES 
-   ('Landscaper', 20.00),
-   ('Construction Worker', 25.00),
-   ('Electrician', 30.00),
-   ('Plumber', 30.00),
-   ('Farmhand', 15.00),
-   ('Community watch', 18.00),
-   ('Waste Management Worker', 20.00),
-   ('Road Maintenance Worker', 22.00);
- 
+	
+	
 INSERT INTO JobRequests (JobRequestDescription, UserID, ImageReferenceID, JobTypeID, EstimatedCost) 
 VALUES 
 	('Installing a fence around a park', 1, 1, 1, 100.00),
@@ -81,33 +81,48 @@ VALUES
 	('Cutting grass near a local school', 7, 7, 2, 75.00),
 	('Building a library', 8, 8, 3, 250.00),
 	('Neighborhood garbage collection', 9, 9, 4, 150.00),
-	('Fixing/closing potholes', 10, 10, 5, 200.00);
+	('Fixing/closing potholes', 10, 10, 5, 200.00);	
+	
+	
+	
   
 INSERT INTO Donations (Amount, UserID, JobRequestID)
 VALUES 
-   (150.00, 1, 20),
-   (220.00, 2, 19),
-   (100.00, 3, 18),
-   (400.00, 4, 17),
-   (30.00, 5, 16),
-   (75.00, 6, 15),
-   (150.00, 7, 12),
-   (40.00, 8, 11),
-   (50.00, 9, 13),
-   (325.00, 10, 20);
+   (150.00, 1, 1),
+   (220.00, 2, 2),
+   (100.00, 3, 3),
+   (400.00, 4, 4),
+   (30.00, 5, 5),
+   (75.00, 6, 6),
+   (150.00, 7, 7),
+   (40.00, 8, 8),
+   (50.00, 9, 9),
+   (325.00, 10, 10);
+  
+
+
+
   
 INSERT INTO PublicVotes (UserID, JobRequestID, UpVotes, DownVotes)
 VALUES 
-   (1, 20, 10, 2),
-   (2, 19, 5, 1),
-   (3, 18, 8, 0),
-   (4, 17, 2, 0),
-   (5, 16, 3, 1),
-   (6, 15, 7, 3),
-   (7, 14, 1, 0),
-   (8, 13, 0, 2),
-   (9, 12, 4, 0),
-   (10, 11, 6, 10);
+   (1, 1, 10, 2),
+   (2, 2, 5, 1),
+   (3, 3, 8, 0),
+   (4, 3, 2, 0),
+   (5, 4, 3, 1),
+   (6, 5, 7, 3),
+   (7, 6, 1, 0),
+   (8, 4, 0, 2),
+   (9, 9, 4, 0),
+   (10, 10, 6, 10);
+   
+
+INSERT INTO Statuses (StatusName)
+VALUES
+	('Complete'),
+	('In Progress'),
+	('Incomplete');
+	
   
 INSERT INTO ApprovedJobs (JobRequestID, UserID, StatusID)
 VALUES 
@@ -122,18 +137,18 @@ VALUES
    (19, 9, 2),
    (20, 10, 3);
    
-INSERT INTO JobImageReferences (ApprovedJobID, ImageReferenceID)
+INSERT INTO ApprovedJobs (JobRequestID, UserID, StatusID)
 VALUES 
-   (1, 1),
-   (2, 2),
-   (3, 3),
-   (4, 4),
-   (5, 5),
-   (6, 6),
-   (7, 7),
-   (8, 8),
-   (9, 9),
-   (10, 10);
+   (1, 1, 1),
+   (2, 2, 1),
+   (3, 3, 2),
+   (4, 4, 2),
+   (5, 5, 3),
+   (5, 6, 3),
+   (7, 7, 3),
+   (8, 8, 1),
+   (9, 9, 2),
+   (6, 10, 3);
 
 
  
