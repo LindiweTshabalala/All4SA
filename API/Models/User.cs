62a9c34b5578f11fb2866b576325822c91a39c15
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2023-02-27 12:11:30 +0200

    implementation for the user model

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string Surname { get; set; }
+        public string idNumber { get; set; }
+        public string token { get; set; }
     }
 }
