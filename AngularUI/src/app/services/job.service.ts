20d62e2dca4307371b38c0c7235f0fd9d54b70d2
Lesedi <lesedijonas12@gmail.com>
2023-02-27 23:47:03 +0200

    add display for all jobs

--- AngularUI/src/app/services/job.service.ts
+++ AngularUI/src/app/services/job.service.ts
@@ -13,11 +13,26 @@ export class JobService {
 
   constructor(private http: HttpClient) {}
 
-  getJobs(): Observable<Job[]> {
-    return this.http.get<Job[]>(this.url)
-      .pipe(
-        tap((_) => console.log('fetched Users')),
-        catchError(this.handleError<User[]>('getUsers', []))
-      );
+  getJob(): Observable<Job[]> {
+    console.log(this.url+'/JobRequests');
+    return this.http.get<Job[]>(this.url+'/JobRequests')
+      // .pipe(
+      //   tap((_) => console.log('fetched JobRequests')),
+      //   catchError(this.handleError<Job[]>('getJobs', []))
+      // );
+  }
+  
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
