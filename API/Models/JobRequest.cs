f3dc3ec3e631e0e2a1e668b426a32254d938ddfa
amaze <amaze.mkhonto2@gmail>
2023-02-27 14:46:29 +0200

    Get All jobRequests

--- API/Models/JobRequest.cs
+++ API/Models/JobRequest.cs
@@ -2,5 +2,12 @@
 {
     public class JobRequest
     {
+        public int JobRequestID { get; set; }
+        public string JobRequestDescription { get; set; }
+        public int UserID { get; set; }
+        public int ImageReferenceID { get; set; }
+        public int JobTypeID { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public bool Status { get; set; }
     }
 }
