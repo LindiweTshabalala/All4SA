190cd93f9ec9ca341444d4a0fcbde5975f412100
amaze <amaze.mkhonto2@gmail>
2023-02-28 17:06:18 +0200

    procedure to insert into users, contacts and addresses

--- SQL-scripts/create_procedures_scripts.sql
+++ SQL-scripts/create_procedures_scripts.sql
@@ -27,3 +27,46 @@ END;
 $$;
 
 CALL add_donation_amount(CAST(102.69 AS MONEY),1);
+
+
+
+CREATE OR REPLACE PROCEDURE sp_add_user(
+    IN first_name VARCHAR(120),
+    IN surname VARCHAR(120),
+    IN id_number CHAR(13),
+    IN token_ VARCHAR(255),
+    IN email VARCHAR(120),
+    IN cell_phone VARCHAR(20),
+    IN province VARCHAR(50),
+    IN city VARCHAR(50),
+    IN street_name VARCHAR(50),
+    IN postal_code INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    user_id INT;
+BEGIN
+   
+    BEGIN
+      
+        INSERT INTO Users (FirstName, Surname, IdNumber, token) 
+			   VALUES (first_name, surname, id_number, token_) RETURNING UserID INTO user_id;
+
+        INSERT INTO Contacts (Email, CellPhone, UserID) 
+			   VALUES (email, cell_phone, user_id);
+
+        INSERT INTO Addresses (Province, City, StreetName, PostalCode, UserID)
+			  VALUES (province, city, street_name, postal_code, user_id);
+
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Error adding user with contact and address: %', SQLERRM;
+        ROLLBACK;
+        RETURN;
+    END;
+    COMMIT;
+END;
+$$;
+
+call sp_add_user('Done', 'Mashaba', '3505230824520','gldfjdfhdsgfjhdgshfdsagldsf','done@yahoo.com',
+				 '0736256300','North West', 'Porch','Near Me', 1912 );
