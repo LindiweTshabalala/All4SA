72c784933d875c4613b1486074ec362650ecb92a
amaze <amaze.mkhonto2@gmail>
2023-02-28 17:08:47 +0200

    donation model

--- API/Models/Donation.cs
+++ API/Models/Donation.cs
@@ -2,5 +2,9 @@
 {
     public class Donation
     {
+        public int DonationID { get; set; }
+        public decimal Amount { get; set; }
+        public int UserID { get; set; }
+        public int JobRequestID { get; set; }
     }
 }
