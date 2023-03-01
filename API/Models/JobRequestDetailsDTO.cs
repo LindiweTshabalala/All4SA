7dceb0999e45d2d325a818ced15e08631950eb27
Lesedi <lesedijonas12@gmail.com>
2023-03-01 22:49:38 +0200

    add a dto to help transfer data

---
+++ API/Models/JobRequestDetailsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace All4SA.Models
+{
+    public class JobRequestDetailsDTO
+    {
+        public int JobRequestID { get; set; }
+        public string JobRequestDescription { get; set; }
+        public int UserID { get; set; }
+        public string ImageReferenceID { get; set; }
+        public int JobTypeID { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public bool Status { get; set; }
+    }
+}
