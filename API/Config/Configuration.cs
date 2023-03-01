b7a3594d24e14f4c64ced5f768bd99966ee74141
Zwelisha Langa <zwelisha@bbd.co.za>
2023-03-01 22:40:06 +0200

    Added functionality to save database configurations

---
+++ API/Config/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace All4SA.Config
+{
+    internal class Configuration
+    {
+        private static readonly string dbConfig = @".\.dbConfig.json";
+        public string? Host { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? DatabaseName { get; set; }
+
+        public static string SetConfiguration()
+        {
+            try
+            {
+                string text = File.ReadAllText(dbConfig);
+                var configuration = JsonSerializer.Deserialize<Configuration>(text);
+                string connectionString = $"Host={configuration?.Host};Username={configuration?.Username};Password={configuration?.Password};Database={configuration?.DatabaseName};";
+
+                return connectionString;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
