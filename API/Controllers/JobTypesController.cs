9ba04b97191bf3abdd66eb81abb5a43e30ae0fbc
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2023-03-02 08:50:16 +0200

    remove the job types and statuses api that are not implemented

--- API/Controllers/JobTypesController.cs
+++ API/Controllers/JobTypesController.cs
@@ -13,35 +13,5 @@ namespace All4SA.Controllers
         {
             return new ObjectResult(JobTypesCRUD.GetAll());
         }
-
-        [HttpGet("GetJobTypeByID/{jobTypeID}", Name = "GetJobTypeByID")]
-        public IActionResult GetJobTypeByID(int jobTypeID)
-        {
-            return new ObjectResult("");
-        }
-
-        [HttpPost("AddJobType", Name = "AddJobType")]
-        public IActionResult AddJobType(JobType jobType)
-        {
-            return new ObjectResult("");
-        }
-
-        [HttpPut("UpdateJobType/{jobTypeID}", Name = "UpdateJobType")]
-        public IActionResult UpdateJobType(JobType jobType)
-        {
-            return new ObjectResult("");
-        }
-
-        [HttpPut("SoftDeleteJobType/{jobTypeID}", Name = "SoftDeleteJobType")]
-        public IActionResult SoftDeleteJobType(int jobTypeID)
-        {
-            return new ObjectResult("");
-        }
-
-        [HttpDelete("DeleteJobType/{jobTypeID}", Name = "DeleteJobType")]
-        public IActionResult DeleteJobType(int jobTypeID)
-        {
-            return new ObjectResult("");
-        }
     }
 }
