962972de73a692f8f762655d6d44846dce46a212
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2023-03-02 08:15:49 +0200

    remove the contacts api that are not implemented

--- API/Controllers/AddressesController.cs
+++ API/Controllers/AddressesController.cs
@@ -13,37 +13,5 @@ namespace All4SA.Controllers
         {
             return new ObjectResult(AddressesCRUD.GetAll());
         }
-
-
-        //[HttpGet("GetAddressByID/{addressID}", Name = "GetAddressByID")]
-        //public IActionResult GetAddressByID(int addressID)
-        //{
-        //    return new ObjectResult("");
-        //}
-
-        //[HttpPost("AddAddress", Name = "AddAddress")]
-        //public IActionResult AddAddress(Address address)
-        //{
-
-        //    return new ObjectResult("");
-        //}
-
-        //[HttpPut("UpdateAddress/{addressID}", Name = "UpdateAddress")]
-        //public IActionResult UpdateAddress(Address address)
-        //{
-        //    return new ObjectResult("");
-        //}
-
-        //[HttpPut("SoftDeleteAddress/{addressID}", Name = "SoftDeleteAddress")]
-        //public IActionResult SoftDeleteAddress(int addressID)
-        //{
-        //    return new ObjectResult("");
-        //}
-
-        //[HttpDelete("DeleteAddress/{addressID}", Name = "DeleteAddress")]
-        //public IActionResult DeleteAddress(int addressID)
-        //{
-        //    return new ObjectResult("");
-        //}
     }
 }
